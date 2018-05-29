246a4aa0dcd2c01b8fbcae69ce70472a9bde5ae4
Ilya Aleshin <>
2018-05-29 13:40:00 +0300

    Lesson1

---
+++ Label&Constraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Label&Constraints
+//
+//  Created by Александр Бойченко on 22.05.2018.
+//  Copyright © 2018 Bakh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
